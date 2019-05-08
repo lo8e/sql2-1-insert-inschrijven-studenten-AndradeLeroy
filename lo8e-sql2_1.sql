7d248e9b981744e9b4fd126f1d31064b4fc0ffae
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-08 23:14:39 +0200

    2: Studenten aanmaken
    Voeg 4 records toe aan de tabel studenten.
    
    ov_nummer: tussen 90000 en 99999.
    Verzin zelf de rest van de velden

--- lo8e-sql2_1.sql
+++ lo8e-sql2_1.sql
@@ -327,3 +327,10 @@ VALUES ("lo9e-amo1", "vlt03", "2019", "amo"),
 	   ("lo9e-amo2", "rs001", "2019", "amo"),
 	   ("lo9e-amo3", "bkr02", "2019", "amo");
 
+#opdracht2:
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voorletters, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, actief, inschrijvings_datum) 
+VALUES (9000, "Puk", "", "P","Pietje", "1992-02-01", "1000AA", "Amsterdam", "70,4", "1.60", 0, "2019-06-01"),
+(90001, "Puk", "", "P","Pietje", "1992-02-02", "1000AA", "Amsterdam", "70,4", "1.61", 0, "2019-06-01"),
+(90002, "Puk", "", "P","Pietjed", "1992-02-03", "1000AA", "Amsterdam", "70,4", "1.62", 0, "2019-06-01"),
+(90003, "Puk", "", "P","Pietjec", "1992-02-04", "1000AA", "Amsterdam", "70,4", "1.63", 0, "2019-06-01");
+
