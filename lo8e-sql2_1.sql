62d25b054a734b8f31dd7abf6e1740410f4d96e0
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-08 23:43:52 +0200

    4: Wijzigen student
    Een van de net ingeschreven studenten heeft zijn/haar geboortedatum verkeerd opgegeven. Wijzig dit d.m.v. een UPDATE statement.
    
    Update voor student: 90001
    van 1992-02-01 -> 1992-02-02

--- lo8e-sql2_1.sql
+++ lo8e-sql2_1.sql
@@ -334,3 +334,7 @@ VALUES (9000, "Puk", "", "P","Pietje", "1992-02-01", "1000AA", "Amsterdam", "70,
 (90002, "Puk", "", "P","Pietjed", "1992-02-03", "1000AA", "Amsterdam", "70,4", "1.62", 0, "2019-06-01"),
 (90003, "Puk", "", "P","Pietjec", "1992-02-04", "1000AA", "Amsterdam", "70,4", "1.63", 0, "2019-06-01");
 
+
+#Opdracht4:
+UPDATE studenten SET geboortedatum='1992-02-02'
+WHERE ov_nummer = '90001';
