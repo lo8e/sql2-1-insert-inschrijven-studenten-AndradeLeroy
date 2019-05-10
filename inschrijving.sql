e50066680986ac3016486e2300afb935d6a087f7
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-10 17:55:42 +0200

    Opdracht6: pre-finaal
    Toegevoegd: update regel 27

--- inschrijving.sql
+++ inschrijving.sql
@@ -13,8 +13,6 @@ SELECT studenten.ov_nummer, CONCAT(studenten.voornaam, " ", studenten.tussenvoeg
 AS volledigenaam, klassen.klas_code, 
 klassen.cohort, CONCAT(docenten.voorletters," ", docenten.achternaam) AS volledigeDocentenNaam
 
-FROM studenten
-
 INNER JOIN klassen_studenten
 ON klassen_studenten.ov_nummer = studenten.ov_nummer
 
@@ -26,4 +24,4 @@ on docenten.docent_code = klassen.slb_code
 
 WHERE studenten.actief = "1"
 
-ORDER BY  klassen.klas_code ASC, studenten.achternaam ASC;
+ORDER BY klassen.klas_code ASC, studenten.achternaam ASC;
