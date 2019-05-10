c77b6d9422f5cc7b382c69dbbff0a933d3839551
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-10 13:27:24 +0200

    Voorkomen toekomstige error: Data truncated "opleidingen.opleiding_code"
    RESOLVED:
    INSERT INTO opleidingen('opleiding_code')
    VALUES(opleiding_code VARCHAR(32) NOT NUll);
    
    #of
        CREATE TABLE `opleidingen` (
    	`opleiding_code` VARCHAR(3) NOT NULL,
    	`titel` VARCHAR(32) NOT NULL,
        PRIMARY KEY (`opleiding_code`)
    );
    #want applicatie en mediaontwikkeling bevat 32 characters, waarvoorheen 10 en dat zal zorgen voor data truncated

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,1 +1,10 @@
-#Inschrijvingen SQL
+INSERT INTO opleidingen('opleiding_code')
+VALUES(opleiding_code VARCHAR(32) NOT NUll);
+
+#of
+    CREATE TABLE `opleidingen` (
+	`opleiding_code` VARCHAR(3) NOT NULL,
+	`titel` VARCHAR(32) NOT NULL,
+    PRIMARY KEY (`opleiding_code`)
+);
+#want applicatie en mediaontwikkeling bevat 32 characters, waarvoorheen 10 en dat zal zorgen voor data truncated
