fcc7eb462f91a35b5a5de5f972698bef01e8bd7f
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-10 13:29:58 +0200

    1: Klassen aanmaken
    * -> klas_code: lo9e-amo1 t/m lo9e-amo3
    * -> slb_code: elk een eigen slb-docent
    * -> cohort: 2019
    * -> opleiding_code 'amo'

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,10 +1,5 @@
-INSERT INTO opleidingen('opleiding_code')
-VALUES(opleiding_code VARCHAR(32) NOT NUll);
-
-#of
-    CREATE TABLE `opleidingen` (
-	`opleiding_code` VARCHAR(3) NOT NULL,
-	`titel` VARCHAR(32) NOT NULL,
-    PRIMARY KEY (`opleiding_code`)
-);
-#want applicatie en mediaontwikkeling bevat 32 characters, waarvoorheen 10 en dat zal zorgen voor data truncated
+#opdracht:1
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) 
+VALUES ("lo9e-amo1", "vlt03", "2019", "amo"),
+	   ("lo9e-amo2", "brc01", "2019", "amo"),
+	   ("lo9e-amo3", "akr02", "2019", "amo");
