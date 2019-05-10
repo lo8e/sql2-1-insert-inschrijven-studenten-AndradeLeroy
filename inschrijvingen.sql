bb7d27978324cfbe32ee0c581e3edc699b2244f8
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-10 13:36:14 +0200

    3: Koppelen student klas
    # 3 Koppelen student klas
    Voeg 4 records toe aan de tabel klassen_studenten.
    * ->- Zorg dat de studenten die je net hebt toegevoegd elk aan een klas toegevoegd worden.
    * -> Alle klassen moeten tenminste 1 student bevatten.

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,6 @@
-#opdracht2:
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voorletters, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, actief, inschrijvings_datum) 
-VALUES (90000, "Puk", "", "P","Pietje", "1992-02-01", "1000AA", "Amsterdam", "70,4", "1.60", 0, "2019-06-01"),
-(90001, "Puk", "", "P","Pietje", "1992-02-02", "1000AA", "Amsterdam", "70,4", "1.61", 0, "2019-06-01"),
-(90002, "Puk", "", "P","Pietjed", "1992-02-03", "1000AA", "Amsterdam", "70,4", "1.62", 0, "2019-06-01"),
-(90003, "Puk", "", "P","Pietjec", "1992-02-04", "1000AA", "Amsterdam", "70,4", "1.63", 0, "2019-06-01");
+#Opdracht3:
+#INSERT INTO klassen_studenten(klas_code, ov_nummer)
+VALUES("lo9e-amo3", 90000),
+	  ("lo9e-amo1", 90001),
+	  ("lo9e-amo2", 90002),
+	  ("lo9e-amo3", 90003);
