4e62b7343c13e3c8def5882a2d5e961454d9c86c
AndradeLeroy <AndradeLeroy@users.noreply.github.com>
2019-05-10 13:58:20 +0200

    5: Oude studenten inactief
    # Studenten ingeschreven voor 1-1-2017 dienen inactief gezet te worden.
    #* -> Wijzig dit d.m.v een UPDATE statement.UPDATE studenten
    
    SET actief = "0" WHERE inschrijvings_datum <= "2017-1-1";

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,3 +1,3 @@
-#Opgelost: Workbench versie: 5.6.30
-#EDIT -> SQL EDITOR -<(unchecked Safe Updates)-herstart workbench
-
+# Studenten ingeschreven voor 1-1-2017 dienen inactief gezet te worden.
+#* -> Wijzig dit d.m.v een UPDATE statement.UPDATE studenten 
+SET actief = "0" WHERE inschrijvings_datum <= "2017-1-1";
